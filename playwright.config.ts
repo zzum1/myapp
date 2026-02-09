fd65fe49cbd37d2aa42d0a794eeba592b66250ec
zzum1 <simonasstaskevicius@gmail.com>
2026-02-09 14:14:59 +0200

    reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
 
   workers: process.env.CI ? 1 : undefined,
 
-  reporter: "list",
+  reporter: [["list"], ["html", { outputDir: "playwright-report" }]],
 
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
