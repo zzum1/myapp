aff962e1514070e9e5db8fcd9f70e2178afaaaf8
zzum1 <simonasstaskevicius@gmail.com>
2026-02-10 17:39:13 +0200

    only

--- tests/e2e/homePage.spec.ts
+++ tests/e2e/homePage.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import { HomePage } from "../page-objects/homePage.pom";
 
 test.describe("Home Page tests", () => {
-  test.only("should display the sticky header", async ({ page }) => {
+  test("should display the sticky header", async ({ page }) => {
     const homePage = new HomePage(page);
     await homePage.goto();
     await homePage.headerShouldBeVisible();
