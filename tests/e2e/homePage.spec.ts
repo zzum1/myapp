cd524a6f6c5e2f7c900dfafa91a8d0637ade8426
zzum1 <simonasstaskevicius@gmail.com>
2026-02-11 18:13:48 +0200

    fixture cookieConsent

--- tests/e2e/homePage.spec.ts
+++ tests/e2e/homePage.spec.ts
@@ -2,7 +2,6 @@ import { test } from "../fixtures/page-objects-fixtures";
 
 test.describe("Home Page tests", () => {
   test("should display the sticky header", async ({ homePage }) => {
-    await homePage.goto();
     await homePage.headerShouldBeVisible();
   });
 });
--- tests/fixtures/page-objects-fixtures.ts
+++ tests/fixtures/page-objects-fixtures.ts
@@ -4,12 +4,21 @@ import { HomePage } from "../page-objects/homePage.pom";
 // Define a fixture for the HomePage
 type MyFixtures = {
   homePage: HomePage;
+  cookieConsent: void;
 };
 
 export const test = base.extend<MyFixtures>({
   homePage: async ({ page }, use) => {
     const homePage = new HomePage(page);
+    await homePage.goToHomePage();
     await use(homePage);
   },
+  cookieConsent: [
+    async ({ homePage }, use) => {
+      await homePage.clickCookieConsent();
+      await use();
+    },
+    { auto: true },
+  ],
 });
 export { expect } from "@playwright/test";
