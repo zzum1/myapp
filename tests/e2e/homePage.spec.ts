a90939c1d2123b83cd5a4cd4e87238d400c4ff68
zzum1 <simonasstaskevicius@gmail.com>
2026-02-12 11:24:57 +0200

    remove beforeAll

--- tests/e2e/homePage.spec.ts
+++ tests/e2e/homePage.spec.ts
@@ -1,16 +1,16 @@
 import { test } from "../fixtures/page-objects-fixtures";
 
-test.beforeAll(async ({ homePage }) => {
-  await homePage.goToHomePage();
-});
 test.describe("Home Page tests", () => {
   test("should display the sticky header", async ({ homePage }) => {
+    await homePage.goToHomePage();
     await homePage.headerShouldBeVisible();
   });
+
   test("should allow users to click the reservation button", async ({
     homePage,
     loginPage,
   }) => {
+    await homePage.goToHomePage();
     await homePage.clickReservationButton();
     await loginPage.loginFormShouldBeVisible();
   });
