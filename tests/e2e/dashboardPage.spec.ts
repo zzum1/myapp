a04ff25406d1eba45ce33d8eff29d7a949dae7bc
zzum1 <simonasstaskevicius@gmail.com>
2026-02-19 09:03:00 +0200

    only

--- tests/e2e/dashboardPage.spec.ts
+++ tests/e2e/dashboardPage.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "../fixtures/page-objects-fixtures";
 
 test.describe("Dashboard Page tests", () => {
-  test.only("should display dashboard page", async ({ dashboardPage }) => {
+  test("should display dashboard page", async ({ dashboardPage }) => {
     await dashboardPage.userProfileMenuShouldBeVisible();
   });
 });
