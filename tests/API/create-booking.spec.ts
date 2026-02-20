d8c756bd1f37b22b57cf7b2bb5fddb3ad4daa0ec
zzum1 <simonasstaskevicius@gmail.com>
2026-02-20 14:45:00 +0200

    tag

--- tests/API/create-booking.spec.ts
+++ tests/API/create-booking.spec.ts
@@ -5,18 +5,22 @@ import { bookingSchema } from "../../schema-validation/booking-schema";
 test.describe("Create Booking API Test", () => {
   let bookingId: number;
 
-  test("should create a new booking", async ({ request }) => {
-    const newBooking = generateRandomUserData();
+  test(
+    "should create a new booking",
+    { tag: "critical" },
+    async ({ request }) => {
+      const newBooking = generateRandomUserData();
 
-    const response = await request.post("/booking", {
-      data: newBooking,
-    });
+      const response = await request.post("/booking", {
+        data: newBooking,
+      });
 
-    expect(response.status()).toBe(200);
-    const responseBody = await response.json();
+      expect(response.status()).toBe(200);
+      const responseBody = await response.json();
 
-    expect(() => {
-      bookingSchema.parse(responseBody);
-    }).not.toThrow();
-  });
+      expect(() => {
+        bookingSchema.parse(responseBody);
+      }).not.toThrow();
+    },
+  );
 });
